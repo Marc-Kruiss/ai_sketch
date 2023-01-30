613d7f29e7afbc1462f70e2cc184db1285c019e3
Marc Kruiß <k.marc@a1.net>
2023-01-30 15:07:20 +0100

    setup canvas with typescript

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import SketchPad from "../components/SketchPad";
+
 function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <body>
+      <div className="flex flex-col items-center">
+        <div>
+          <h1>Data Creator</h1>
+        </div>
+        <br />
+        <SketchPad height={500} width={500} />
+      </div>
+    </body>
+  );
 }
 
 export default Homepage;
